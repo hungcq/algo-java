1c3f79041240e64360129abf8564aa6db9db9515
HungCQ <hungcq1996@gmail.com>
2020-06-03 08:31:53 +0700

    add tree traversal

---
+++ src/session_8/Node.java
@@ -0,0 +1,15 @@
+package session_8;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node() {
+
+    }
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
