d5c6e89642ea189ec3bffb67cbba48c982e648c6
HungCQ <hungcq@vng.com.vn>
2020-06-17 17:16:29 +0700

    init tree 2-4

--- src/arrays_strings/Test.java
+++ src/arrays_strings/Test.java
@@ -21,11 +21,11 @@ public class Test {
         System.out.println(test.stringCompression("aabbccc"));
 
         int[][] matrix = new int[][]{
-                {1,2,3,4,5},
-                {6,7,8,9,10},
-                {11,12,13,14,15},
-                {16,17,18,19,20},
-                {21,22,23,24,25},
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25},
         };
         test.rotateImage(matrix);
         for (int i = 0; i < matrix.length; i++) {
@@ -90,13 +90,10 @@ public class Test {
         int n = matrix.length;
         for (int i = 0; i < n / 2; i++) {
             for (int j = i; j < n - i - 1; j++) {
-                int j2 = n - i - 1;
-                int j3 = n - j - 1;
-                int j4 = n - j2 - 1;
-                int tmp = matrix[j3][j4];
-                matrix[j3][j4] = matrix[j2][j3];
-                matrix[j2][j3] = matrix[j][j2];
-                matrix[j][j2] = matrix[i][j];
+                int tmp = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = matrix[i][j];
                 matrix[i][j] = tmp;
             }
         }
---
+++ src/session_11/Tree24.java
@@ -0,0 +1,11 @@
+package session_11;
+
+/**
+ * Created by: HungCQ
+ * Date: 17-Jun-20
+ */
+public class Tree24 {
+    class Node {
+
+    }
+}
