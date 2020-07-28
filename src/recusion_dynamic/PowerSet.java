67402286de80deac3750d06de56968f00e64d8b5
HungCQ <hungcq@vng.com.vn>
2020-07-28 14:32:39 +0700

    add power set without dup

--- src/recusion_dynamic/PowerSet.java
+++ src/recusion_dynamic/PowerSet.java
@@ -3,6 +3,7 @@ package recusion_dynamic;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,6 +18,28 @@ public class PowerSet {
         List<List<Integer>> power = powerSet.powerSet(new int[]{1, 4, 6, 62, 4, 21, 5});
         System.out.println(gson.toJson(power));
         System.out.println(power.size());
+    }
+
+    public List<List<Integer>> powerSetWithoutDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        subsetsWithDupHelper(nums, 0, res, new ArrayList<>());
+        return res;
+    }
+
+    private void subsetsWithDupHelper(int[] nums, int pos, List<List<Integer>> res, List<Integer> tmpRes) {
+        // subset means it does not need contain all elements, so the condition is <= rather than ==
+        // and do not return after this statement
+        if(pos <= nums.length) res.add(new ArrayList<>(tmpRes));
+
+        for(int i = pos; i < nums.length; i++) {
+            if (i > pos && nums[i] == nums[i-1]) {
+                continue;   // avoid duplicates
+            }
+            tmpRes.add(nums[i]);
+            subsetsWithDupHelper(nums, i + 1, res, tmpRes);
+            tmpRes.remove(tmpRes.size() - 1);
+        }
     }
 
     List<List<Integer>> powerSet(int[] array) {
